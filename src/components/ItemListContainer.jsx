d3bdb3fa3948397e674e1fba20f4a1784a174555
lutalivre <fsakmexico@gmail.com>
2024-12-23 20:58:45 -0600

    clase8: custm hooks y HOC

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,71 +1,19 @@
-import { useState, useEffect } from "react"
-import { getProducts } from "../data/data.js"
+import hoctFilterProducts from "../hoc/hoctFilterProducts.jsx"
 import ItemList from "./ItemList.jsx"
 import "./itemlistcontainer.css"
 
-
-
-const ItemListContainer = ({ saludo }) => {
-  const [products, setProducts] = useState ([])
-
+const ItemListContainer = ({ saludo, products }) => {
   
 
-  //const condicion = false
-  useEffect(()=>{
-    getProducts()
-    .then((data)=> {
-      setProducts(data)
-    })
-    .catch((error)=>{
-      console.error(error)
-    })
-    .finally(()=> {
-      console.log("termino la promesa")
-    })
-  }, [])
-
-  
-  //? Variable
-  /*const promesa = new Promise ( (resolve, reject) => {
-    if(condicion ===true){
-    resolve("se resolvio")
-     }else{
-      reject("se rechazo")
-    }
-  })
-*/
-  
-  //! Funcion, lo ideal es manejarlo con una Función
-  /*const promesa =() => {
-    return new Promise( (resolve, reject) => {
-      if(condicion === true){
-        resolve("se resolvio")
-      }else{
-        reject("se rechazo")
-      }
-    })
-  }
-  */
-  
- 
-  /*promesa
-    .then((respuesta)=> {
-      console.log(respuesta)
-    })
-    .catch((error)=>{
-      console.error(error)
-    })
-    .finally(()=> {
-      console.log("termino la promesa")
-    })
-  */
-    
   return (
     <div className="itemlistcontainer">
         <h1>{ saludo }</h1>
-        <ItemList products={products}/>
-    </div>
-  )
+        <ItemList products={products} />
+    </div>  
+  ) 
 }
+//export default ItemListContainer
 
-export default ItemListContainer
+const ItemListContainerWithHoc = hoctFilterProducts(ItemListContainer)
+
+export default ItemListContainerWithHoc
